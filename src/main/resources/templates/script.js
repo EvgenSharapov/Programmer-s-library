b0c42eec49c82379fda0827905df60b0bf225bf9
EvgenSharapov <dooplert@yandex.ru>
2025-01-24 22:29:11 +0300

    login/password

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -1,20 +1,3 @@
-// // Получаем элементы DOM
-// const userButton = document.getElementById('user-button');
-// const dropdownMenu = document.getElementById('dropdown-menu');
-//
-// // Обработчик клика по кнопке
-// userButton.addEventListener('click', () => {
-//     dropdownMenu.classList.toggle('show');
-// });
-//
-// // Закрываем меню, если кликнут вне его
-// window.addEventListener('click', (event) => {
-//     if (!event.target.matches('#user-button')) {
-//         if (dropdownMenu.classList.contains('show')) {
-//             dropdownMenu.classList.remove('show');
-//         }
-//     }
-// });
 // Добавляем иконку пользователя в кнопку
 const userButton = document.getElementById('user-button');
 userButton.innerHTML = '<i class="fas fa-user"></i>'; // Иконка пользователя из Font Awesome
